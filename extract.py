5235526224ba2058d1a096d3584b3999969f2826
Gabriel Andrade <gabriel.andrad4@gmail.com>
2020-10-06 23:40:15 -0300

    Multiprocessing

--- extract.py
+++ extract.py
@@ -15,8 +15,16 @@ def unzip(path, fileName):
 
 def convertToCsv(filepath, fileName):
     print ("Convertendo arquivo Excel")
-    wb = pd.ExcelFile(filepath)
-    wb = wb.parse(sheetname=wb.sheet_names[0], index_col=None, na_values=['NA'])
+    # wb = pd.ExcelFile(filepath)
+    # wb = wb.parse(sheetname=wb.sheet_names[0], index_col=None, na_values=['NA'])
+    # newFileName = os.path.join(
+    #         'data', fileName.split('.')[0] + '.' + 'csv')
+    # wb.to_csv(newFileName, index=False)
+
+    teste = pd.read_excel(filepath)
+    teste['Partida Prevista'] = teste['Partida Prevista'].dt.strftime('%d/%m/%Y %H:%M')
+    teste['Partida Real'] = teste['Partida Real'].dt.strftime('%d/%m/%Y %H:%M')
     newFileName = os.path.join(
             'data', fileName.split('.')[0] + '.' + 'csv')
-    wb.to_csv(newFileName, index=False)
+    teste.to_csv(newFileName, index=False)
+    teste.to_csv(newFileName, index=False)
